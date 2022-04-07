e1ab50b37c5d923c767a2a8b26b917035db5c17d
extramix <sirapat.jr@gmail.com>
2022-04-07 22:11:25 +0700

    add Swagger

--- server.js
+++ server.js
@@ -10,13 +10,35 @@ const auth = require("./routes/auth");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const xss = require("xss-clean");
-const rateLimit = require("express-rate-limit");
+
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
 
 const app = express();
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "Hospital API Documentation",
+    },
+    servers: [
+      {
+        url: "http://localhost:5050/api/v1",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use(express.json());
 
@@ -29,17 +51,11 @@ app.use(helmet());
 
 app.use(xss());
 
-const limiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 mins
-  max: 1,
-});
-app.use(limiter);
-
 app.use("/api/v1/hospitals", hospitals);
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/appointments", appointments);
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5050;
 const server = app.listen(
   PORT,
   console.log("Server running in ", process.env.NODE_ENV, " mode on port", PORT)
